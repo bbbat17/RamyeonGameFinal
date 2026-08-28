149a59cca1faaa3064e97d2f0ba79c0393e6c471
bbbat17 <bbbat@naver.com>
2026-08-28 13:34:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -661,7 +661,7 @@ function startBoiling(waterElement, potElement) {
 
   const waitTime = (waterElement.boilWaitRemaining !== undefined && waterElement.boilWaitRemaining !== null)
     ? waterElement.boilWaitRemaining
-    : ((waterElement.accumulatedBoilTime > 0) ? 0 : 1000);
+    : ((waterElement.accumulatedBoilTime > 0) ? 0 : 000);
 
   if (waitTime > 0) {
     waterElement.isBoilWaiting = true;
